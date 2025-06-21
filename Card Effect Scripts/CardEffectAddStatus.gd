eafa7b5550fdd2e432c6ad6b50e807d44a55b878
Eric Hsieh <eric.kf.hsieh@gmail.com>
2025-06-20 17:26:16 -0700

    create card that consumes motes to gain effect

--- Card Effect Scripts/CardEffectAddStatus.gd
+++ Card Effect Scripts/CardEffectAddStatus.gd
@@ -11,5 +11,5 @@ func apply_effect() -> void:
 	StatusManager.apply_status(statusType, counter)
 
 func _get_description() -> String:
-	var counter_text = str(counter) + " " if counter > 0  else ""
-	return "Gain " + counter_text + Status.type_to_string(statusType)
+	var counter_text = str(counter) + " " if counter != 0  else ""
+	return "Gain " + counter_text + Status.type_to_string(statusType) 
